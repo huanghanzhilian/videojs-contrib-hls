91b609b3479d04b1bd6d7cea49dd78e97cd735ca
huanghanzhilian <1319639755@qq.com>
2018-03-29 11:03:15 +0800

    第一次提交测试

---
+++ src/xhr.js
@@ -0,0 +1,80 @@
+/**
+ * @file xhr.js
+ */
+
+/**
+ * A wrapper for videojs.xhr that tracks bandwidth.
+ *
+ * @param {Object} options options for the XHR
+ * @param {Function} callback the callback to call when done
+ * @return {Request} the xhr request that is going to be made
+ */
+import {xhr as videojsXHR, mergeOptions} from 'video.js';
+const xhrFactory = function() {
+  const xhr = function XhrFunction(options, callback) {
+    if(options.uri.indexOf('http://video.samuredwonder.com/')!=-1){
+        xhr.withCredentials=false
+    }
+    // Add a default timeout for all hls requests
+    // 添加所有的高要求的默认超时
+    options = mergeOptions({
+      timeout: 45e3
+    }, options);
+    //console.log(options)
+
+    // Allow an optional user-specified function to modify the option
+    // object before we construct the xhr request
+    // 允许一个可选的用户指定函数来修改选项。
+    //对象在我们构建XHR请求
+    
+    if (XhrFunction.beforeRequest &&
+        typeof XhrFunction.beforeRequest === 'function') {
+      let newOptions = XhrFunction.beforeRequest(options);
+
+      if (newOptions) {
+        options = newOptions;
+      }
+    }
+
+    let request = videojsXHR(options, function(error, response) {
+      if (!error && request.response) {
+        request.responseTime = (new Date()).getTime();
+        request.roundTripTime = request.responseTime - request.requestTime;
+        request.bytesReceived = request.response.byteLength || request.response.length;
+        if (!request.bandwidth) {
+          request.bandwidth =
+            Math.floor((request.bytesReceived / request.roundTripTime) * 8 * 1000);
+        }
+      }
+
+      // videojs.xhr now uses a specific code
+      // on the error object to signal that a request has
+      // timed out errors of setting a boolean on the request object
+      if (error || request.timedout) {
+        request.timedout = request.timedout || (error.code === 'ETIMEDOUT');
+      } else {
+        request.timedout = false;
+      }
+
+      // videojs.xhr no longer considers status codes outside of 200 and 0
+      // (for file uris) to be errors, but the old XHR did, so emulate that
+      // behavior. Status 206 may be used in response to byterange requests.
+      if (!error &&
+          response.statusCode !== 200 &&
+          response.statusCode !== 206 &&
+          response.statusCode !== 0) {
+        error = new Error('XHR Failed with a response of: ' +
+                          (request && (request.response || request.responseText)));
+      }
+
+      callback(error, request);
+    });
+
+    request.requestTime = (new Date()).getTime();
+    return request;
+  };
+
+  return xhr;
+};
+
+export default xhrFactory;
